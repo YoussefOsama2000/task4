33223977841b014b1e434c247b2616ede331caa8
YoussefOsama200 <youssef.ali001@eng-st.cu.edu.eg>
2021-12-23 02:14:23 +0200

    dummy app start

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: DummyPage(),
+  ));
+}
+
+class DummyPage extends StatefulWidget {
+  const DummyPage({Key? key}) : super(key: key);
+
+  @override
+  _DummyPageState createState() => _DummyPageState();
+}
+
+
+class _DummyPageState extends State<DummyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white60,
+        body: Column(
+          children: [Text("asdfa")],
+        ));
+  }
+}
